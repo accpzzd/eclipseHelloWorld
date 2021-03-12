a9c5c89597ec98ea6375c7564c2de467926d5347
java1234 <java1234@qq.com>
2021-03-12 11:22:10 +0800

    11111

--- src/eclipseHelloWorld/HelloWorld.java
+++ src/eclipseHelloWorld/HelloWorld.java
@@ -4,7 +4,6 @@ public class HelloWorld {
 	
 	public static void main(String[] args) {
 		System.out.println("hello World !");
-		System.out.println("新增的333333");
 		System.out.println("over1");
 		System.out.println("over2");
 		System.out.println("over3");
